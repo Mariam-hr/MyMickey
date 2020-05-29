8a3eca8900b1e696ade8b95111e08679d24faf73
Mariam Baghdasaryan <mariambaghdasaryan@Annas-MacBook-Pro-2.local>
2020-05-29 13:23:00 +0400

    EggTimer

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    
+    var secondsRemaining = 60
+    
+    var timer = Timer()
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle!
+        
+        secondsRemaining = eggTimes[hardness]!
+        
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        }
+
+
+
+        @objc func updateCounter() {
+            //example functionality
+            if secondsRemaining > 0 {
+                print("\(secondsRemaining) seconds")
+                secondsRemaining -= 1
+            } else {
+                timer.invalidate()
+                titleLabel.text = "DONE!"
+            }
+        }
+        
+}
+
+
